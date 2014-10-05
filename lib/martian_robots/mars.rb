a9b7a96995d16a37273157c1c55238cf75b6e094
mariogintili <mariojgintili@gmail.com>
2014-10-05 19:33:42 +0100

    covered all contexts for a robots move request

--- lib/martian_robots/mars.rb
+++ lib/martian_robots/mars.rb
@@ -9,7 +9,95 @@ module MartianRobots
       @grid = build_grid args[:coordinates]
     end
 
+    def forbidden_coordinates
+      @forbidden_coordinates ||= []
+    end
+
+    def upgrade_location(robot)
+      new_coords = robot.next_coordinate
+
+      if forbidden_coordinates.include?(new_coords)
+        robot.ignore!
+
+      elsif off_edge?(new_coords)
+        mark_lost(robot)
+
+      else
+        robot.move
+        set(robot, new_coords)
+      end
+    end
+
+    def valid?(coords)
+      valid_x = grid.transpose.length >= coords.first
+      valid_y = grid.length >= coords.last
+      valid_x && valid_y 
+    end
+
+    def safe_area?(coords)
+      find_by_coordinate(coords) != forbidden
+    end
+
+    def off_edge?(coords)
+      !valid?(coords)
+    end
+
+    def find_by_coordinate(coordinates)
+      grid[coordinates[0]][coordinates[1]] rescue nil
+    end
+
+    def coordinates_for(element)      
+      x = grid.find_index { |row| row.include? element }
+      y = grid.transpose.find_index { |row| row.include? element }
+      return if [x,y].any? { |a| a.nil? }
+      [x, y] 
+    end
+
+    def set(element, new_coordinates)
+      old_coordinates = coordinates_for(element)
+      delete_by_coordinate(old_coordinates)
+      insert(new_coordinates, element)
+    end
+
+    def find(element)
+      grid.find(-> {[]}) { |row| row.include? element }.find { |a| a == element }
+    end
+
     private
+
+    def insert(coords, value)
+      set_value(coords, value)
+    end
+
+    def mark_lost(robot)
+      found_robot = find(robot)
+      found_robot.vanish
+      delete_by_coordinate(found_robot.coordinates)
+      forbidden_coordinates.push found_robot.coordinates
+    end
+
+    def delete_by_coordinate(coords)
+      set_value(coords, nil)
+    end
+
+    def x_limit
+      grid.length
+    end
+
+    def y_limit
+      grid.transpose.length
+    end
+
+    def substract_if_limit(coords)
+      x = coords[0] - 1 if coords[0] >= x_limit
+      y = coords[1] - 1 if coords[1] >= y_limit
+      [x || coords[0], y || coords[1]] 
+    end
+
+    def set_value(coords, value)
+      coordinates = substract_if_limit(coords)
+      grid[coordinates[0]][coordinates[1]] = value
+    end
 
     def build_grid(coordinates)
       raise "A grid cant exceel a value of 50" if coordinates.max > 50
--- spec/martian_robots/mars_spec.rb
+++ spec/martian_robots/mars_spec.rb
@@ -18,4 +18,121 @@ describe MartianRobots::Mars do
       expect { Mars.new(coordinates: [30, 51]) }.to raise_error
     end
   end
+
+  describe "#find" do
+
+    context "with a found entry" do
+
+      it "returns the found element" do
+        subject.grid[1][1] = robot
+        expect(subject.find(robot)).to eq robot
+      end
+    end
+
+    context "without a found entry" do
+
+      it "returns nil" do
+        expect(subject.find(robot)).to eq nil
+      end
+    end
+  end
+
+  describe "#coordinates_for" do
+
+    context "with a found entry" do
+
+      it "returns the coordinates of the given object" do
+        subject.grid[1][1] = robot
+        expect(subject.coordinates_for(robot)).to eq [1,1]
+      end
+    end
+
+    context "not found entry" do
+
+      it "returns nil" do
+        expect(subject.coordinates_for(robot)).to eq nil
+      end
+    end
+  end
+
+  describe "#find_by_coordinate" do
+
+    context "with an entry" do
+
+      it "returns the coordinates" do
+        subject.grid[1][1] = robot
+        expect(subject.find_by_coordinate([1,1])).to eq robot
+      end
+    end
+
+    context "without an entry" do
+
+      it "returns nil" do
+        expect(subject.find_by_coordinate([6,6])).to eq nil
+        expect(subject.find_by_coordinate([2,2])).to eq nil
+      end
+
+    end
+  end
+
+  describe "#set" do
+
+    it "updates an object's location on the grid" do
+      subject.grid[0][0] = robot
+      subject.set(robot, [1,1])
+      expect(subject.grid[0][0]).not_to eq robot
+      expect(subject.coordinates_for(robot)).to eq [1,1]
+    end
+  end
+
+  describe "#valid?" do
+
+    it "returns false if the coordinate is outside of the grid" do
+      expect(subject.valid?([10,10])).to be_falsey
+      expect(subject.valid?([3,10])).to be_falsey
+      expect(subject.valid?([10,3])).to be_falsey
+      expect(subject.valid?([5,3])).to be_truthy
+      expect(subject.valid?([0,0])).to be_truthy
+      expect(subject.valid?([3,4])).to be_falsey
+    end
+  end
+
+  describe "#upgrade_location" do
+
+    let(:robot) { double('robot',coordinates: [0,0], next_coordinate: next_coordinates, vanish: nil) }
+
+    context "when it's forbidden" do
+
+      let(:next_coordinates) { [1,1] }
+
+      it "tells the robot to ignore future movement" do
+        subject.forbidden_coordinates.push next_coordinates
+        subject.grid[0][0] = robot
+        expect(robot).to receive(:ignore!)
+        subject.upgrade_location(robot)
+      end
+    end
+
+    context "when it's off the edge" do
+
+      let(:next_coordinates) { [3,4] }
+
+      it "marks the robot as lost" do
+        subject.grid[2][3] = robot
+        expect(robot).to receive(:vanish)
+        subject.upgrade_location(robot)
+      end
+    end
+
+    context "under valid circumstances" do
+
+      let(:next_coordinates) { [2,2] }
+
+      it "tells the robot to move" do
+        subject.grid[1][1] = robot
+        expect(robot).to receive(:move)
+        subject.upgrade_location(robot)
+      end 
+    end
+  end
 end
